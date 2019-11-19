d1c936fcc5cc5e89cdcf09c08aed22a730c545bc
Nikita Silivonchik <lstpsche@gmail.com>
2019-11-19 22:08:59 +0300

    #SR-63 fix abc issue at start handler

--- app/handlers/text_commands/start_handler.rb
+++ app/handlers/text_commands/start_handler.rb
@@ -9,14 +9,24 @@ module Handlers
 
       def handle
         if user_registered?(id: chat_id)
-          show_welcome_message if user.first_start_message?
-          show_main_menu
+          start_registered
         else
-          register_user
-          show_welcome_message
-          show_otp_generation_question
-          show_help_message
+          start_not_registered
         end
+      end
+
+      private
+
+      def start_registered
+        show_welcome_message if user.first_start_message?
+        show_main_menu
+      end
+
+      def start_not_registered
+        register_user
+        show_welcome_message
+        show_otp_generation_question
+        show_help_message
       end
     end
   end
